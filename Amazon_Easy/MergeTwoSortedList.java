9a052b320166ecb64156c576aa4fc8afe7c7a044
wasiahmad <alexxwasi95@gmail.com>
2025-07-19 18:46:24 +0530

    Merge Two Sorted List Solution Added

---
+++ Amazon_Easy/MergeTwoSortedList.java
@@ -0,0 +1,63 @@
+// ----- Problem Statement -----
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+
+package DSA_Library.Amazon_Easy;
+import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class MergeTwoSortedList {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val > l2.val) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+
+        ListNode mergedHead = l1;
+        while (l1 != null && l2 != null) {
+            ListNode temp = null;
+            while (l1 != null && l1.val <= l2.val) {
+                temp = l1;
+                l1 = l1.next;
+            }
+            temp.next = l2;
+
+            // Swap l1 and l2
+            ListNode swap = l1;
+            l1 = l2;
+            l2 = swap;
+        }
+
+        return mergedHead;
+    }
+
+    // Helper function to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Example usage
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode mergedList = mergeTwoLists(l1, l2);
+        printList(mergedList);
+    }
+}
