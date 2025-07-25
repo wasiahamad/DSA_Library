528750163e6b5cdba68f89f46620463e3ce2b0fb
wasiahmad <alexxwasi95@gmail.com>
2025-07-25 17:37:51 +0530

    sumSubArrayRange solution Added

---
+++ Amazon_Medium/SumOfSubArrayRange.java
@@ -0,0 +1,26 @@
+package DSA_Library.Amazon_Medium;
+
+public class SumOfSubArrayRange {
+    // function of SumOfSubArrayRange
+
+    public static int sumRange(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int max = arr[i];
+            int min = arr[i];
+            for (int j = i; j < arr.length; j++) {
+                max = Math.max(max, arr[j]);
+                min = Math.min(min, arr[j]);
+                ans += max - min;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+        int sum = sumRange(arr);
+        System.out.println(sum);
+    }
+}
