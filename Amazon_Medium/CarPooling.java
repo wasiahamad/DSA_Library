f5981dfd5e8cc663fe26e75fd801abe77a7e4606
wasiahmad <alexxwasi95@gmail.com>
2025-07-26 18:06:26 +0530

    Commit the carPolling problem

--- Amazon_Medium/CarPooling.java
+++ Amazon_Medium/CarPooling.java
@@ -1,3 +1,6 @@
+//  Problme statement: 
+
+
 package DSA_Library.Amazon_Medium;
 
 public class CarPooling {
@@ -5,8 +8,8 @@ public class CarPooling {
     public static Boolean carPooling(int trips[][], int capicity) {
         int m[] = new int[1001];
         for (int a[] : trips) {
-            m[1] += a[0];
-            m[2] -= a[0]; 
+            m[a[1]] += a[0];
+            m[a[2]] -= a[0]; 
         }
 
         for (int i = 0; i < 1001 && capicity >= 0; i++) {
