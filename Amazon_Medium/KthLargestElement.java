8c781e2f67f0ed82103d62ce822c8d0a2c395068
wasiahmad <alexxwasi95@gmail.com>
2025-08-07 17:42:46 +0530

    Kth largest element problem is Added

---
+++ Amazon_Medium/KthLargestElement.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    // ------------------- Approch:- 1 (using Priority Queue) ------------------find
+    // KthLargestElement
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+
+    // ------------------- Approch:- 2 (using two pointer) ------------------find
+    // KthLargestElement\
+    public static int findKthLargestUsingTwoPoniter(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (right - left + 1 > k) { 
+            if (nums[left] < nums[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 1, 5, 6, 4 };
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));
+    }
+}
