6c2f44a6bd2e81165d43967a82866413f74bdf86
wasiahmad <alexxwasi95@gmail.com>
2025-08-07 17:44:05 +0530

    find Kth Largest Using Two Poniter

--- Amazon_Medium/KthLargestElement.java
+++ Amazon_Medium/KthLargestElement.java
@@ -33,5 +33,6 @@ public class KthLargestElement {
         int[] nums = { 3, 2, 1, 5, 6, 4 };
         int k = 2;
         System.out.println(findKthLargest(nums, k));
+        System.out.println(findKthLargestUsingTwoPoniter(nums, k));
     }
 }
